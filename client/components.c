24a2b4ab8469230852dfd8efd0fe72285fb5cd87
LuisPeMoraRod <luismorarod98@gmail.com>
2021-04-21 19:57:48 -0600

    Add enemies.png

--- client/components.c
+++ client/components.c
@@ -6,10 +6,18 @@
 
 struct player{
     SDL_Rect pos;
-    SDL_Texture *texture;
+    SDL_Texture *current_texture;
+    SDL_Texture *run_right0;
+    SDL_Texture *run_right1;
+    SDL_Texture *run_left0;
+    SDL_Texture *run_left1;
     char image_path[MAX_PATH];
     int lives;
     bool right_direction;
     bool liana;
 };
+
+struct player *create_player(){
+    SDL_Texture
+}
 
